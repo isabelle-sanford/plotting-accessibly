26ba8c2c29103efad2606c465f7588b3367a16a6
Isabelle Sanford <isabellesanford@gmail.com>
2022-07-12 09:38:48 -0400

    color, resizing, maybe alt text

--- stuff/prev.js
+++ stuff/prev.js
@@ -1,58 +1,3 @@
-let tvData = [
-  { label: "Han", value: 33 },
-  { label: "Christian", value: 12 },
-  { label: "Lisa", value: 41 },
-  { label: "Jacob", value: 16 },
-  { label: "Nick", value: 59 },
-  { label: "Ahmed", value: 38 },
-  { label: "Peleke", value: 21 },
-  { label: "Matt", value: 25 },
-];
-let tvAnnotations = {
-  title: "Title Test",
-  xAxis: "X Axis",
-  yAxis: "Y Axis",
-  caption:
-    "Caption. This is where a caption would go, and would normally be a sentence or two in length. How do I make the text wrap instead of running off the paaaage",
-  // colorScale: "",
-  // altText: "A graph of TV watchers or whatever",
-};
-let sleepData = [
-  { label: "12am", value: 0 },
-  { label: "1am", value: 2 },
-  { label: "2am", value: 0 },
-  { label: "3am", value: 0 },
-  { label: "4am", value: 3 },
-  { label: "5am", value: 5 },
-  { label: "6am", value: 3 },
-  { label: "7am", value: 8 },
-  { label: "8am", value: 14 },
-  { label: "9am", value: 9 },
-  { label: "10am", value: 6 },
-  { label: "11am", value: 5 },
-  { label: "12pm", value: 9 },
-  { label: "1pm", value: 4 },
-  { label: "2pm", value: 3 },
-  { label: "3pm", value: 4 },
-  { label: "4pm", value: 1 },
-  { label: "5pm", value: 3 },
-  { label: "6pm", value: 1 },
-  { label: "7pm", value: 4 },
-  { label: "8pm", value: 2 },
-  { label: "9pm", value: 2 },
-  { label: "10pm", value: 3 },
-  { label: "11pm", value: 0 },
-];
-
-let sleepAnnotations = {
-  title: "How often I woke up at each hour of the day April-June 2022",
-  xAxis: "Time",
-  yAxis: "Number of Occurrences",
-  caption: "Nope",
-  // maybe rename to just "color"
-  colorScale: "vibrant", // must be option name OR color list
-};
-
 // COLOR LIST
 const colors = {
   bright: ["#47A", "#6ce", "#283", "#cb4", "#e67", "#a37", "#bbb"],
@@ -75,32 +20,75 @@ const colors = {
   ],
 };
 
-// Define SVG area dimensions
-const svgWidth = 1000;
-const svgHeight = 800;
+// // Define SVG area dimensions
+// const svgWidth = 1000;
+// const svgHeight = 1000;
 
-// Define the chart's margins as an object
-const chartMargin = {
-  top: 50,
-  right: 30,
-  bottom: 150,
-  left: 100,
-};
-
-// Define dimensions of the chart area
-let chartWidth = svgWidth - chartMargin.left - chartMargin.right;
-let chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+// // Define the chart's margins as an object
+// const chartMargin = {
+//   top: 50,
+//   right: 30,
+//   bottom: 150,
+//   left: 100,
+// };
 
 // data should be [{"label": "x", "value": y}]
 function makeGraph(data, divId, annotations) {
   console.log(data);
+
+  let div_w = d3
+    .select("#" + divId)
+    .style("width")
+    .split("px")
+    .shift();
+  // ! use?
+  let div_h = d3
+    .select("#" + divId)
+    .style("height")
+    .split("px")
+    .shift();
+
+  // ? is using only width always okay?
+  let svgHeight = div_w * 0.8;
+  let svgWidth = div_w;
+
+  let chartMargin = {
+    top: 0.1 * svgHeight,
+    bottom: 0.2 * svgHeight,
+    right: 0.05 * svgWidth,
+    left: 0.1 * svgWidth,
+  };
+
+  // Define dimensions of the chart area
+  let chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+  let chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+  console.log(
+    `SVG is ${svgHeight} by ${svgWidth}, chart is ${chartHeight} by ${chartWidth}`
+  );
 
   // Select body, append SVG area to it, and set the dimensions
   let svg = d3
     .select("#" + divId)
     .append("svg")
     .attr("height", svgHeight)
-    .attr("width", svgWidth);
+    .attr("width", svgWidth)
+    .attr("viewbox", `0 0 ${svgWidth} ${svgHeight}`)
+    .attr("role", "img")
+    .attr("aria-labelledby", `title-${divId} `)
+    .attr("aria-describedby", `desc-${divId}`)
+    .attr("class", "PlotSVG")
+    .attr("color", "green");
+  // .attr("aria-label", "title+desc"); // ?
+
+  svg
+    .append("title")
+    .attr("text", annotations.title)
+    .attr("id", `title-${divId} `);
+  svg
+    .append("description")
+    .attr("text", annotations.caption)
+    .attr("id", `desc-${divId}`); // ? alt text?
 
   // Append a group to the SVG area and shift ('translate') it to the right and to the bottom
   let chartGroup = svg
@@ -117,7 +105,7 @@ function makeGraph(data, divId, annotations) {
   // Create a linear scale for the vertical axis.
   let yLinearScale = d3
     .scaleLinear()
-    .domain([0, d3.max(data, (d) => d.value)])
+    .domain([0, d3.max(data, (d) => d.value)]) // ! customizable?
     .range([chartHeight, 0]);
 
   // Create two new functions passing our scales in as arguments
@@ -127,12 +115,13 @@ function makeGraph(data, divId, annotations) {
 
   // Append two SVG group elements to the chartGroup area,
   // and create the bottom and left axes inside of them
-  chartGroup.append("g").call(leftAxis);
+  chartGroup.append("g").call(leftAxis).style("font-size", "14px"); // ! align with other text - style not size?
 
   chartGroup
     .append("g")
     .attr("transform", `translate(0, ${chartHeight})`)
-    .call(bottomAxis);
+    .call(bottomAxis)
+    .style("font-size", "14px");
 
   // COLOR
   let colorChoice = colors["highContrast"]; // tbh default should probs be just like blue
@@ -141,8 +130,6 @@ function makeGraph(data, divId, annotations) {
   } else if (annotations.colorScale.length > 0) {
     colorChoice = annotations.colorScale;
   }
-  console.log(annotations.colorScale + "  " + annotations.colorScale.length);
-  console.log(colorChoice);
 
   // Create one SVG rectangle per piece of data
   // Use the linear and band scales to position each rectangle within the chart
@@ -164,38 +151,147 @@ function makeGraph(data, divId, annotations) {
   chartGroup
     .append("text")
     .attr("transform", "rotate(-90)")
-    .attr("y", 0 - chartMargin.left + 40)
-    .attr("x", 0 - chartHeight / 2)
-    .attr("dy", "1em")
+    .attr("y", -chartMargin.left / 2)
+    .attr("x", -chartHeight / 2)
+    //.attr("dy", "1em") // ??
     .attr("class", "axisText")
-    .text(annotations.yAxis);
+    .text(annotations.yAxis)
+    .style("text-anchor", "middle");
 
-  // x axis
+  // x axis // todo maybe add px minimum height?
   chartGroup
     .append("text")
     .attr(
       "transform",
-      `translate(${chartWidth / 2}, ${chartHeight + chartMargin.top})`
+      `translate(${chartWidth / 2}, ${chartHeight + chartMargin.top})` // ??
     )
     .attr("class", "axisText")
-    .text(annotations.xAxis);
+    .text(annotations.xAxis)
+    .style("text-anchor", "middle");
 
   // title
   chartGroup
     .append("text")
-    .attr("transform", `translate(${20}, ${-chartMargin.top / 2})`)
+    .attr("transform", `translate(${chartWidth / 2}, ${-chartMargin.top / 2})`)
     .attr("class", "axisText")
-    .text(annotations.title);
+    .text(annotations.title)
+    .style("text-anchor", "middle");
 
-  // caption
+  // caption // ! wrap
   chartGroup
     .append("text")
     .attr(
       "transform",
-      `translate(${chartMargin.left}, ${chartHeight + chartMargin.top + 50})`
+      `translate(${chartWidth / 2}, ${svgHeight - chartMargin.bottom / 2})`
     )
-    .attr("class", "axisText")
-    .text(annotations.caption);
+    .attr("class", "axisText") // ??? no?
+    .text(annotations.caption)
+    .style("text-anchor", "middle");
 }
 
-makeGraph(sleepData, "graph", sleepAnnotations);
+const wakeTimeData = [
+  { label: 0, value: 6 },
+  { label: 1, value: 4 },
+  { label: 2, value: 2 },
+  { label: 3, value: 2 },
+  { label: 4, value: 12 },
+  { label: 5, value: 5 },
+  { label: 6, value: 15 },
+  { label: 7, value: 38 },
+  { label: 8, value: 62 },
+  { label: 9, value: 56 },
+  { label: 10, value: 24 },
+  { label: 11, value: 27 },
+  { label: 12, value: 29 },
+  { label: 13, value: 20 },
+  { label: 14, value: 10 },
+  { label: 15, value: 6 },
+  { label: 16, value: 10 },
+  { label: 17, value: 8 },
+  { label: 18, value: 9 },
+  { label: 19, value: 12 },
+  { label: 20, value: 6 },
+  { label: 21, value: 6 },
+  { label: 22, value: 4 },
+  { label: 23, value: 2 },
+];
+const wakeAnnotations = {
+  title: "Waking Time Frequency (2021-22)",
+  xAxis: "Hour of the day",
+  yAxis: "Number of Occurrences",
+  caption:
+    "Created from a near-complete record of my sleep from June 2021 to June 2022. When Mr. Bilbo Baggins of Bag End announced that he would be celebrating his eleventy-first birthday with a party of special magnificence, there was much talk and excitement in Hobbiton.",
+  colorScale: "vibrant",
+};
+
+const sleepDurationData = [
+  { label: 1, value: 13 },
+  { label: 2, value: 9 },
+  { label: 3, value: 19 },
+  { label: 4, value: 31 },
+  { label: 5, value: 17 },
+  { label: 6, value: 30 },
+  { label: 7, value: 53 },
+  { label: 8, value: 47 },
+  { label: 9, value: 53 },
+  { label: 10, value: 31 },
+  { label: 11, value: 16 },
+  { label: 12, value: 15 },
+  { label: 13, value: 8 },
+  { label: 14, value: 9 },
+  { label: 15, value: 6 },
+  { label: 16, value: 4 },
+  { label: 17, value: 4 },
+  { label: 18, value: 2 },
+  { label: 19, value: 4 },
+  { label: 21, value: 2 },
+  { label: 22, value: 2 },
+];
+
+const durationAnnotations = {
+  title: "Sleep Duration (2021-22)",
+  xAxis: "Time",
+  yAxis: "Number of Occurrences",
+  caption:
+    "Created from a near-complete record of my sleep from June 2021 to June 2022.",
+  colorScale: "bright",
+};
+const sleepStartData = [
+  { label: 12, value: 3 },
+  { label: 13, value: 2 },
+  { label: 14, value: 3 },
+  { label: 15, value: 10 },
+  { label: 16, value: 10 },
+  { label: 17, value: 6 },
+  { label: 18, value: 4 },
+  { label: 19, value: 3 },
+  { label: 20, value: 2 },
+  { label: 21, value: 5 },
+  { label: 22, value: 20 },
+  { label: 23, value: 37 },
+  { label: 0, value: 79 },
+  { label: 1, value: 49 },
+  { label: 2, value: 41 },
+  { label: 3, value: 26 },
+  { label: 4, value: 22 },
+  { label: 5, value: 15 },
+  { label: 6, value: 13 },
+  { label: 7, value: 6 },
+  { label: 8, value: 6 },
+  { label: 9, value: 7 },
+  { label: 10, value: 4 },
+  { label: 11, value: 2 },
+];
+const sleepAnnotations = {
+  title: "Sleeping Time Frequency (2021-22)",
+  xAxis: "Hour of the day",
+  yAxis: "Number of Occurrences",
+  caption:
+    "Created from a near-complete record of my sleep from June 2021 to June 2022.",
+  colorScale: "muted",
+};
+
+makeGraph(wakeTimeData, "graph", wakeAnnotations);
+makeGraph(wakeTimeData, "graph1", wakeAnnotations);
+makeGraph(sleepStartData, "graph3", sleepAnnotations);
+makeGraph(sleepDurationData, "graph2", durationAnnotations);
